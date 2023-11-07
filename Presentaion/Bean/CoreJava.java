ae48a462f44000f4e76cf1cbf899cc219d15b6fb
kirankamm <118911532+kirankamm@users.noreply.github.com>
2023-11-08 00:50:03 +0530

    Add files via upload

---
+++ Presentaion/Bean/CoreJava.java
@@ -0,0 +1,40 @@
+=Change the value in Method: Java supports only call by value. So, if we pass a primitive
+ value, it will not change the original value. 
+ =But, if we convert the primitive value in an object, it will change the original value.
+
+=Serialization: We need to convert the objects into streams to perform the serialization. If we have a primitive value, we can convert it in objects through the wrapper classes.
+Synchronization: Java synchronization works with objects in Multithreading.
+
+java.util package: The java.util package provides the utility classes to deal with 
+objects.
+Collection Framework: Java collection framework works with objects only.
+ All classes of the collection framework (ArrayList, LinkedList, Vector, HashSet,
+ LinkedHashSet, TreeSet, PriorityQueue, ArrayDeque, etc.) deal with objects only.
+
+
+Unboxing
+The automatic conversion of wrapper type into its corresponding primitive type is known
+ as unboxing. It is the reverse process of autoboxing. Since Java 5, we do not need to 
+ use the intValue() method of wrapper classes to convert the wrapper type into primitives
+ .
+
+
+
+There are six ways to create an object in Java.
+
+1: Create Object in Java Using new Keyword.
+2: Create Object in Java Using newInstance() method.
+3: Create Object in Java Using newInstance() method of Constructor Class.
+4: Create Object in Java Using clone() Method.
+5: Create Object in Java Using Deserialization Method.
+
+
+
+
+
+
+
+
+
+
+
